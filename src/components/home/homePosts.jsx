40aaf0b2a8ec8e5be5f95ece3f76f765dffe466e
ferlobo1985 <fernando.lobo.prez@gmail.com>
2023-10-31 02:04:20 -0300

    08-Home Masonry
    08-Home Masonry

--- src/components/home/homePosts.jsx
+++ src/components/home/homePosts.jsx
@@ -1,7 +1,10 @@
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchPosts } from "../../store/utils/thunks";
-import { Button } from 'react-bootstrap';
+import { Button, Spinner } from 'react-bootstrap';
+import { LinkContainer } from 'react-router-bootstrap';
+
+import Masonry from 'react-masonry-css';
 
 
 const HomePosts = () =>{
@@ -10,7 +13,9 @@ const HomePosts = () =>{
 
 
     useEffect(()=>{
-        dispatch(fetchPosts({page:1,order:"desc",limit:6}))
+        if(homePosts.articles.items.length <= 0){
+            dispatch(fetchPosts({page:1,order:"desc",limit:6}))
+        }
     },[])
 
 
@@ -22,6 +27,42 @@ const HomePosts = () =>{
 
     return(
         <>
+            <Masonry
+                breakpointCols={{default:3,800:2,400:1}}
+                className="my-masonry-grid"
+                columnClassName="my-masonry-grid_column"          
+            >
+                { homePosts.articles ?
+                    homePosts.articles.items.map(item =>(
+                        <div key={item.id}>
+                            <img
+                                style={{width:'100%',height:'200px'}}
+                                src={`${item.image}?${item.id}`}
+                                alt="some pic"
+                            />
+                            <div className="author">
+                                <span>{item.author} - </span>
+                                {item.createdAt}
+                            </div>
+                            <div className="content">
+                                <div className="title">{item.title}</div>
+                                <div className="excerpt">{item.excerpt}</div>
+                                <LinkContainer to={`/article/${item.id}`} className="mt-3">
+                                    <Button variant="light">Read more</Button>
+                                </LinkContainer>
+                            </div>
+
+                        </div>
+                    ))
+                :null}
+            </Masonry>
+            {homePosts.loading ? 
+                <div style={{textAlign:'center'}}>
+                    <Spinner animation="border" role="status">
+                        <span className='visually-hidden'>Loading...</span>
+                    </Spinner>
+                </div>
+            :null}
             {!homePosts.articles.end && !homePosts.loading ? 
                  <Button variant="outline-dark" onClick={()=> loadMorePosts()}>
                     Load More Post
